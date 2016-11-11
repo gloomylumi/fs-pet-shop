2be0383e9268dac4e36f44bc2634612c3eaf94f4
Ariana Clark <ariana.clark@gmail.com>
2016-11-11 13:26:24 -0700

    Add create argument

--- pets.js
+++ pets.js
@@ -16,12 +16,42 @@ if ( cmd === 'read' ) {
     }
     var index = process.argv[ 3 ];
     var pets = JSON.parse( data );
-
-    if ( pets[ index ] ) {
+    if ( !index ) {
+      console.log( pets );
+    } else if ( pets[ index ] ) {
       console.log( pets[ index ] );
     } else {
       console.error( `Usage: ${node} ${file} read INDEX` );
     }
+  } );
+} else if ( cmd === 'create' ) {
+  fs.readFile( petsPath, 'utf8', function( readErr, data ) {
+    if ( readErr ) {
+      throw readErr;
+
+    }
+    var pets = JSON.parse( data )
+    var age = process.argv[ 3 ];
+    var kind = process.argv[ 4 ];
+    var name = process.argv[ 5 ];
+    if ( !age || !kind || !name ) {
+      console.error( `Usage: ${node} ${file} create AGE KIND NAME` );
+      process.exit( 1 );
+    }
+    var pet = {
+      'age': ( parseInt( age ) ),
+      'kind': kind,
+      'name': name
+    };
+    pets.push( pet );
+    var petsJSON = JSON.stringify( pets );
+
+    fs.writeFile( petsPath, petsJSON, function( writeErr ) {
+      if ( writeErr ) {
+        throw writeErr;
+      }
+      console.log( pet );
+    } );
   } );
 } else {
   console.error( `Usage: ${node} ${file} [read | create | update | destroy]` );
