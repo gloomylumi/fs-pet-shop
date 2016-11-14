3c98d1dd7f6fe6329e8471ae932811d401c7e207
Ariana Clark <ariana.clark@gmail.com>
2016-11-13 17:26:32 -0700

    Send all guest records from the database

--- expressServer.js
+++ expressServer.js
@@ -1,4 +1,7 @@
 'use strict';
+const fs = require( 'fs' );
+const path = require( 'path' );
+var petsPath = path.join( __dirname, 'pets.json' );
 
 const express = require( 'express' );
 const app = express();
@@ -6,19 +9,21 @@ var port = process.env.PORT || 8000;
 
 app.disable( 'x-powered-by' );
 
-app.use( function( req, res ) {
-  var pets = [ {
-    "age": 7,
-    "kind": "rainbow",
-    "name": "fido"
-  }, {
-    "age": 5,
-    "kind": "snake",
-    "name": "Buttons"
-  } ];
-  res.send( pets );
-} );
+app.get( '/pets', function( req, res ) {
+  fs.readFile( petsPath, 'utf8', function( err, petsJSON ) {
+    if ( err ) {
+      console.error( err.stack );
+      return res.sendStatus( 500 );
+    }
 
+    var pets = JSON.parse( petsJSON );
+    res.send( pets );
+  } );
+
+} );
+app.use( function( req, res ) {
+  res.sendStatus( 404 );
+} );
 app.listen( port, function() {
   console.log( 'Listening on port', port );
 } );
