eb8e5e81f61a3779c1cd3f586a4b1b000974ae20
Ariana Clark <ariana.clark@gmail.com>
2016-11-13 17:38:46 -0700

    Send individual pets records from the database

--- expressServer.js
+++ expressServer.js
@@ -19,7 +19,22 @@ app.get( '/pets', function( req, res ) {
     var pets = JSON.parse( petsJSON );
     res.send( pets );
   } );
+} );
+app.get( '/pets/:id', function( req, res ) {
+  fs.readFile( petsPath, 'utf8', function( err, petsJSON ) {
+    if ( err ) {
+      console.error( err.stack );
+      return res.sendStatus( 500 );
+    }
+    var id = Number.parseInt( req.params.id );
+    var pets = JSON.parse( petsJSON );
 
+    if ( id < 0 || id >= pets.length || Number.isNaN( id ) ) {
+      return res.sendStatus( 404 );
+    }
+    res.set( 'Content-Type', 'text/plain' );
+    res.send( pets[ id ] );
+  } );
 } );
 app.use( function( req, res ) {
   res.sendStatus( 404 );
@@ -27,3 +42,5 @@ app.use( function( req, res ) {
 app.listen( port, function() {
   console.log( 'Listening on port', port );
 } );
+
+module.exports = app;
